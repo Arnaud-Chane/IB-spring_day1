bc6835f92cadd63a9f5c1367a15401f63a0d5904
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-11 14:18:03 +0100

    Create Book model

--- src/main/java/com/springboot/bookstore/model/book/BookModel.java
+++ src/main/java/com/springboot/bookstore/model/book/BookModel.java
@@ -1,4 +1,26 @@
 package com.springboot.bookstore.model.book;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "book")
 public class BookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer book_id;
+    private String title;
+    private String author;
+    private Integer release_year;
 }
+
+
+//Créez une entité Livre avec les attributs suivants : id (Long), titre (String), auteur (String), et année de publication (Integer).
