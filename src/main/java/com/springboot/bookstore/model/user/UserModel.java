dd2b52dbdc95bd70a6b69faa547c54a54176193f
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-03-11 14:27:12 +0100

    Create UserModel

---
+++ src/main/java/com/springboot/bookstore/model/user/UserModel.java
@@ -0,0 +1,57 @@
+package com.springboot.bookstore.model.user;
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+
+@Entity
+@Table(name = "user")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+    private String lastname;
+    private String firstname;
+    private String email;
+
+    public UserModel() {}
+    public UserModel(String lastname, String firstname, String email) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.email = email;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+//Créez une entité Utilisateur avec les attributs suivants : id (Long), nom (String), prénom (String), et email (String).
+
